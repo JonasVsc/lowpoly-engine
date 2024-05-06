04b895086b33cb07bbe55c8af47002f389e508c7
JonasVsc <jonasvasconcelosbarbosa@gmail.com>
2024-05-06 02:10:02 -0300

    project init, vcpkg, cmake and 3rdparty

---
+++ lowpoly-engine.cpp
@@ -0,0 +1,50 @@
+﻿// engine.cpp : Defines the entry point for the application.
+//
+#include "lowpoly-engine.h"
+
+int main()
+{
+	if (!glfwInit())
+		std::cerr << "Error::glfw" << std::endl;
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "OpenGL Application", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplOpenGL3_Init();
+
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwPollEvents();
+
+		// (Your code calls glfwPollEvents())
+		// ...
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::SetNextWindowPos(ImVec2(500, 500));
+		ImGui::Text("Here you are, the unawared one");
+		glfwSetWindowShouldClose(window, ImGui::Button("Exit", ImVec2(100, 20)));
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+
+	}
+	return 0;
+}
---
+++ lowpoly-engine.h
@@ -0,0 +1,15 @@
+﻿// engine.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include<GL\glew.h>
+#include<GLFW\glfw3.h>
+
+#include<imgui.h>
+#include<imgui_impl_glfw.h>
+#include<imgui_impl_opengl3.h>
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
